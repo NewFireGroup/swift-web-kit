4d9497f81357b3dd1e69f65845956921252a2cf5
Dave Boster <dave@boster.dev>
2025-12-15 13:22:51 -0600

    Enhance WebBrowserView to fetch and display page description using Open Graph meta tags

--- Sources/swift-web-kit/API/UI/Browser/WebBrowserView.swift
+++ Sources/swift-web-kit/API/UI/Browser/WebBrowserView.swift
@@ -9,16 +9,49 @@ import WebKit
 import NFGFoundation
 
 public struct WebBrowserView: View {
+    @State private var page = WebPage()
+    @State private var parsedData: String? = nil
     private var website: (any Website)?
     
     public var body: some View {
-        WebView(url: URL(string: website?.url ?? ""))
-//        VStack {
-//            if let website {
-//                Text("Selected: \(website.name)")
-//                Text("Selected: \(website.url)")
-//            }
-//        }
+        VStack {
+            WebView(url: URL(string: website?.url ?? ""))
+                .onChange(of: page.url, initial: true) { newURL, oldUrl in
+                    Task {
+                        self.parsedData = await self.getPageDescription(page)
+                    }
+                }
+            if let data = parsedData {
+                Text("\(data)")
+            }
+            
+        }
+    }
+    
+    private func getPageDescription(_ page: WebPage) async -> String? {
+        
+        let fetchOpenGraphProperty = """
+            const propertyValues = document.querySelector(`meta[name="${name}"]`);
+            if (propertyValues !== null) {
+                return propertyValues.content;
+            } else {
+                return null
+            }
+            
+        """
+
+        let arguments: [String: String] = [
+            "name": "description"
+        ]
+        
+        do {
+            let description = try await page.callJavaScript(fetchOpenGraphProperty, arguments: arguments) as? String
+            return description
+        } catch (let error) {
+            print("error calling JS: ", error)
+        }
+
+        return nil
     }
     
     public init(_ website: (any Website)? = nil) {
